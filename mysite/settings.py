9b6ee95c4d9e7e498cf65b65bc0c0bab357a3185
annnab-2222 <nyamhannan846@gmail.com>
2025-06-16 16:04:43 +0300

    ADD FILE

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-146q$jrp-$1xcd$=pdvzv3=p&gvoi!y0%lh!u3lu$2^vtizr25
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nabstar.pythonanywhere.com']
 
 
 # Application definition
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
